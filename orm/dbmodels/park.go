f7afb2db8030f5c0ad3c06ff9909e57868a2b23e
coddo <codobanclaudiu@gmail.com>
2016-05-21 01:47:25 +0300

    Added dbmodels for parks, slots and positions (points)

---
+++ orm/dbmodels/park.go
@@ -0,0 +1,29 @@
+package dbmodels
+
+import "gopkg.in/mgo.v2/bson"
+
+// Park represents an entire parking lot, which has one or more slots in which cars are parked
+type Park struct {
+	ID       bson.ObjectId `bson:"_id" json:"id"`
+	Address  string        `bson:"address" json:"address"`
+	Position Point         `bson:"position" json:"position"`
+}
+
+// Equal compares two Park objects. Implements the Objecter interface
+func (park Park) Equal(obj Objecter) bool {
+	otherPark, ok := obj.(Park)
+	if !ok {
+		return false
+	}
+
+	switch {
+	case park.ID != otherPark.ID:
+		return false
+	case park.Address != otherPark.Address:
+		return false
+	case !park.Position.Equal(otherPark.Position):
+		return false
+	}
+
+	return true
+}
---
+++ orm/dbmodels/point.go
@@ -0,0 +1,24 @@
+package dbmodels
+
+// Point contains the left-upper and bottom-lower points of a rectangle in which an entire zone is located
+type Point struct {
+	X float64 `bson:"x" json:"x"`
+	Y float64 `bson:"Y" json:"Y"`
+}
+
+// Equal compares two Point objects. Implements the Objecter interface
+func (point Point) Equal(obj Objecter) bool {
+	otherPoint, ok := obj.(Point)
+	if !ok {
+		return false
+	}
+
+	switch {
+	case point.X != otherPoint.X:
+		return false
+	case point.Y != otherPoint.Y:
+		return false
+	}
+
+	return true
+}
