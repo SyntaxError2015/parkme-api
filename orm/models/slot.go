27f66b5ee0436aeaf73cee577b77a7c765cdba24
coddo <codobanclaudiu@gmail.com>
2016-05-22 00:21:04 +0300

    Added status field to the slots

--- orm/models/slot.go
+++ orm/models/slot.go
@@ -9,30 +9,30 @@ import (
 
 // Slot reprents a square in a parking lot, where the car is parked
 type Slot struct {
-	ID       bson.ObjectId `json:"id"`
-	Park     Park          `json:"park"`
-	Position Point         `json:"position"`
+	ID         bson.ObjectId `json:"id"`
+	Park       Park          `json:"park"`
+	Position   Point         `json:"position"`
+	IsOccupied bool          `json:"isOccupied"`
 }
 
 // Expand copies the dbmodels.Slot to a Slot expands all
 // the components by fetching them from the database
 func (slot *Slot) Expand(dbSlot dbmodels.Slot) {
 	slot.ID = dbSlot.ID
+	slot.IsOccupied = dbSlot.IsOccupied
 	slot.Position.Expand(dbSlot.Position)
 
 	dbPark, _ := parkservice.Get(dbSlot.ParkID)
-	park := Park{}
-	park.Expand(*dbPark)
-
-	slot.Park = park
+	slot.Park.Expand(*dbPark)
 }
 
 // Collapse coppies the Slot to a dbmodels.Slot user and
 // only keeps the unique identifiers from the inner components
 func (slot *Slot) Collapse() *dbmodels.Slot {
 	dbslot := dbmodels.Slot{
-		ID:     slot.ID,
-		ParkID: slot.Park.ID,
+		ID:         slot.ID,
+		ParkID:     slot.Park.ID,
+		IsOccupied: slot.IsOccupied,
 	}
 
 	var position = slot.Position.Collapse()
